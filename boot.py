9447fa602f66007950aaa0645b321f3f1b69a4ca
sianniello <steno87@hotmail.com>
2018-01-12 21:21:48 +0100

    booting

--- boot.py
+++ boot.py
@@ -1,18 +1,4 @@
-import machine
-from network import WLAN
+import weather
 
-# configure the WLAN subsystem in station mode (the default is AP)
-wlan = WLAN(mode=WLAN.STA)
-# go for fixed IP settings
-wlan.ifconfig(config=('192.168.0.5', '255.255.255.0', '192.168.0.1', '8.8.8.8'))
-wlan.scan()     # scan for available networks
-wlan.connect(ssid='NETGEAR55', auth=(WLAN.WPA2, 'phobiccoconut688'))
-while not wlan.isconnected():
-    pass
-print(wlan.ifconfig())
-# enable wake on WLAN
-wlan.irq(trigger=WLAN.ANY_EVENT, wake=machine.SLEEP)
-# go to sleep
-machine.sleep()
-# now, connect to the FTP or the Telnet server and the WiPy will wake-up
+weather.run()
 
