8ecd58d81ed383fde92de06bc8d18c323752889f
sianniello <steno87@hotmail.com>
2018-01-25 13:51:43 +0100

    continue

--- lib/logging.py
+++ lib/logging.py
@@ -1,10 +1,21 @@
 import machine
+from uos import listdir
+
+fileName = 'log.txt'
 
 
 def logging(msg):
     print(msg)
-    with open('log.txt', 'a') as the_file:
-        ts = machine.RTC().now()
-        str_ts = '{:02d}-{:02d}-{:02d} {:02d}:{:02d}'.format(ts[0], ts[1], ts[2], ts[3], ts[4])
-        the_file.write(msg + '\t\t{0}\r\n'.format(str_ts))
-        the_file.close()
+    if listdir(fileName):
+        with open(fileName, 'a') as the_file:
+            ts = machine.RTC().now()
+            str_ts = '{:02d}-{:02d}-{:02d} {:02d}:{:02d}'.format(ts[0], ts[1], ts[2], ts[3], ts[4])
+            the_file.write('{0:50}{1}\r\n'.format(msg, str_ts))
+            the_file.close()
+    else:
+        with open(fileName, 'w') as the_file:
+            ts = machine.RTC().now()
+            str_ts = '{:02d}-{:02d}-{:02d} {:02d}:{:02d}'.format(ts[0], ts[1], ts[2], ts[3], ts[4])
+            the_file.write('{0:50}{1}'.format('Log Message', 'Timestamp\r\n\n'))
+            the_file.write('{0:50}{1}\r\n'.format(msg, str_ts))
+            the_file.close()
