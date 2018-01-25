98ba7bedcaedc2852ed3ec14dd69dc363e900b0a
sianniello <steno87@hotmail.com>
2018-01-25 20:48:58 +0100

    logging

--- lib/logging.py
+++ lib/logging.py
@@ -16,6 +16,6 @@ def logging(msg):
         with open(fileName, 'w') as the_file:
             ts = machine.RTC().now()
             str_ts = '{:02d}-{:02d}-{:02d} {:02d}:{:02d}'.format(ts[0], ts[1], ts[2], ts[3], ts[4])
-            the_file.write('{0:50}{1}'.format('Log Message', 'Timestamp\r\n\n'))
+            the_file.write('{0:50}{1}'.format('LOG MESSAGE', 'TIMESTAMP\r\n\r\n'))
             the_file.write('{0:50}{1}\r\n'.format(msg, str_ts))
             the_file.close()
