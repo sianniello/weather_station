d7f445aef3fce5f5590caf9b127c93b6e2d032c6
sianniello <steno87@hotmail.com>
2018-01-26 20:23:47 +0100

    boot

--- boot.py
+++ boot.py
@@ -1,18 +1,12 @@
-from machine import UART
+from machine import UART, RTC, idle
 import os
-import utime
-import machine
 from network import WLAN
-from microWebCli import MicroWebCli
 from logging import logging
 
 uart = UART(0, 115200)
 os.dupterm(uart)
 
-
 _SSID = "NETGEAR55"
-_TIMEZONE_URL = 'http://api.timezonedb.com/v2/get-time-zone?key=1QNTARL4R9XW&by=zone&zone=Europe/Rome&format=json'
-
 
 wlan = WLAN(mode=WLAN.STA)
 wlan.ifconfig(config=('192.168.0.5', '255.255.255.0', '192.168.0.1', '8.8.8.8'))
@@ -20,15 +14,13 @@ wlan.ifconfig(config=('192.168.0.5', '255.255.255.0', '192.168.0.1', '8.8.8.8'))
 if not wlan.isconnected():
     wlan.connect(_SSID, auth=(WLAN.WPA2, 'phobiccoconut688'), timeout=5000)
     while not wlan.isconnected():
-        machine.idle()  # save power while waiting
+        idle()  # save power while waiting
 
 logging('WLAN connection succeeded!')
 logging("My IP address is: {0}".format(wlan.ifconfig()[0]))
 
 try:
-    contentBytes = MicroWebCli.JSONRequest(_TIMEZONE_URL)
-    tuple_data = contentBytes['timestamp']
-    machine.RTC(datetime=utime.localtime(tuple_data))
+    RTC().ntp_sync("0.it.pool.ntp.org")
     logging("Real Time Clock updated.")
 except Exception as e:
     logging("Real Time Clock updating failed. Error: {0}".format(e))
